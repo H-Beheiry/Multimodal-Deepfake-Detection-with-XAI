d5df9e53e1764af1f9517e33a43b2f77578c963c
Hamza El-Beheiry <hamza.mohamed@ufe.edu.eg>
2026-01-13 04:06:22 +0200

    changed video processing for loading num of samples and anlyzed distrbution on duration

--- Preprocessing/VideoPreprocessor.py
+++ Preprocessing/VideoPreprocessor.py
@@ -10,14 +10,16 @@ from torchvision.io import read_video
 # ])
 
 class VideoHandler():
-    def __init__(self,transformation,num_frames,device):
+    def __init__(self,transformation,tau,fps,device):
         self.device= device
         self.transformation= transformation
-        self.num_frames= num_frames
+        self.tau= tau
+        self.fps= fps
+        self.num_frames= int(tau*fps)
 
     def preprocess(self,file_path):
         vid,_,_= read_video(file_path, pts_unit="sec")
-        vid= self.ensure_frame(vid)
+        vid= self.sample_frames(vid)
         vid= vid.float() /255.0
         vid= vid.permute(0, 3, 1, 2)
         vid= vid.to(self.device)
@@ -25,12 +27,10 @@ class VideoHandler():
         vid= vid.permute(3, 0, 1, 2)        # (T,H,W,C)
         return vid
     
-    def ensure_frame(self,vid):
-        vid_frames= vid.shape[0]
-        if vid_frames < self.num_frames:
-            missing= self.num_frames-vid_frames
-            padding= torch.zeros((missing, *video.shape[1:]), dtype=video.dtype)
-            video= torch.cat((video, padding), dim=0)
-        elif vid_frames > self.num_frames:
-            vid= vid[:self.num_frames]
+    def sample_frames(self,vid):
+        total_frames= vid.shape[0]
+        max_start= total_frames - self.num_frames
+        start_frame= random.randint(0, max_start)
+        end_frame= start_frame + self.num_frames
+        vid= vid[start_frame:end_frame]
         return vid

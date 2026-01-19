a71ba99ca758bd9ca4ae96af9283caeae5e06765
Hamza El-Beheiry <hamza.mohamed@ufe.edu.eg>
2026-01-19 04:30:08 +0200

    added av training and testing

--- DetectionModels/AVModel.py
+++ DetectionModels/AVModel.py
@@ -12,21 +12,23 @@ class AVModel(nn.Module):
             param.requires_grad= False
         for param in self.audio_model.parameters():
             param.requires_grad= False
-        self.gate_layer = nn.Sequential(
-            nn.Linear(self.fusion_input_dim, self.fusion_input_dim),
-            nn.Sigmoid()
-        )
+            
+        self.video_norm = nn.LayerNorm(64)
+        self.audio_norm = nn.LayerNorm(128)
+        
         self.fusion_mlp= nn.Sequential(
-            nn.Linear(self.fusion_input_dim, 64),
-            nn.LayerNorm(64),
-            nn.ReLU(),
+            nn.Linear(self.fusion_input_dim, 512),
+            nn.LayerNorm(512),
+            nn.GELU(),
             nn.Dropout(0.6),
-            nn.Linear(64, num_classes)
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, video_input, audio_input):
         vid_embed= self.video_model(video_input, return_embedding=True) # (B, 64)
         aud_embed= self.audio_model.get_embedding(audio_input) # (B, 128)
+        vid_embed= self.video_norm(vid_embed)
+        aud_embed= self.audio_norm(aud_embed)
         combined_features= torch.cat((vid_embed, aud_embed), dim=1)
         logits= self.fusion_mlp(combined_features)
         return logits
